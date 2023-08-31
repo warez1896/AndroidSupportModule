16158050d6971f45804dce98c4edfc6c976e310e
Raul Armstrong <nonamesback@gmail.com>
2023-08-31 21:27:33 +0800

    Go back

--- supportmodule/src/main/java/com/ediwow/supportmodule/backend/MainAlertDialogFactory.java
+++ supportmodule/src/main/java/com/ediwow/supportmodule/backend/MainAlertDialogFactory.java
@@ -1,21 +1,20 @@
 package com.ediwow.supportmodule.backend;
 
 
-import android.content.Context;
+import android.app.Activity;
 import android.content.DialogInterface;
 
 import androidx.appcompat.app.AlertDialog;
 
 public class MainAlertDialogFactory {
-    private final Context context;
+    private final Activity activity;
 
-    public MainAlertDialogFactory(Context context) {
-        this.context = context;
+    public MainAlertDialogFactory(Activity activity) {
+        this.activity = activity;
     }
 
-
     public AlertDialog.Builder customDialog(String title, String message, boolean canceleable) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
         builder.setTitle(title);
         builder.setMessage(message);
         builder.setCancelable(canceleable);
@@ -23,7 +22,7 @@ public class MainAlertDialogFactory {
     }
 
     public AlertDialog.Builder customSelectionDialog(String title, String[] choices, int selectedItem, DialogInterface.OnClickListener listener, boolean canceleable) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
         builder.setTitle(title);
         builder.setSingleChoiceItems(choices, selectedItem, listener);
         builder.setCancelable(canceleable);
@@ -31,7 +30,7 @@ public class MainAlertDialogFactory {
     }
 
     public AlertDialog.Builder pleaseWait(String message) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
         builder.setTitle("Please wait");
         builder.setMessage(message);
         builder.setCancelable(false);
