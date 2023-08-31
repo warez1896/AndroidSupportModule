9221a5672dc5f3a5cdef2056ed592377908b2201
Raul Armstrong <nonamesback@gmail.com>
2023-08-31 21:21:13 +0800

    Switch to context

--- supportmodule/src/main/java/com/ediwow/supportmodule/backend/MainAlertDialogFactory.java
+++ supportmodule/src/main/java/com/ediwow/supportmodule/backend/MainAlertDialogFactory.java
@@ -1,24 +1,21 @@
 package com.ediwow.supportmodule.backend;
 
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.DialogInterface;
 
 import androidx.appcompat.app.AlertDialog;
 
 public class MainAlertDialogFactory {
-    private final Activity activity;
+    private final Context context;
 
-    public MainAlertDialogFactory(Activity activity) {
-        this.activity = activity;
+    public MainAlertDialogFactory(Context context) {
+        this.context = context;
     }
 
-    public Activity getActivity() {
-        return this.activity;
-    }
 
     public AlertDialog.Builder customDialog(String title, String message, boolean canceleable) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setTitle(title);
         builder.setMessage(message);
         builder.setCancelable(canceleable);
@@ -26,7 +23,7 @@ public class MainAlertDialogFactory {
     }
 
     public AlertDialog.Builder customSelectionDialog(String title, String[] choices, int selectedItem, DialogInterface.OnClickListener listener, boolean canceleable) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setTitle(title);
         builder.setSingleChoiceItems(choices, selectedItem, listener);
         builder.setCancelable(canceleable);
@@ -34,7 +31,7 @@ public class MainAlertDialogFactory {
     }
 
     public AlertDialog.Builder pleaseWait(String message) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setTitle("Please wait");
         builder.setMessage(message);
         builder.setCancelable(false);
